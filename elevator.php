2edd420e06b7748e3b04e0ec5a2f49b3218fbfd3
RouanLaporal <rouan.laporal@outlook.com>
2021-11-13 21:11:02 +0100

    add some comments to elevator.php

--- elevator.php
+++ elevator.php
@@ -113,44 +113,67 @@ class Elevator
     }
 
 
+    /**
+     * Sends the elevator to the floor selected or called& if the floor is upper than the actual floor of the elevator
+     * @param int $floor Floor selected or called by the user
+     * 
+     * @return void
+     */
     public function goUp(int $floor){
-        while ($this->floor() != $floor) {
-            $this->setDirection('top');
-            print('elevator direction : ' . $this->direction() . "\n");
+        if($this->floor() > $floor)
+            $this->goDown($floor);
+        else{
+            while ($this->floor() != $floor) {
+                $this->setDirection('top');
+                $this->setGate('close');
+                $this->setSignal('start');
+                $this->setfloor($this->floor() + 1);
+                if ($this->floor() == $this->floorMax())
+                    $this->setDirection('bottom');
+            }
+            $this->setSignal('stop');
+            $this->setGate('open');
+            sleep(20);
             $this->setGate('close');
-            print('elevator gate : ' . $this->gate() . "\n");
-            $this->setSignal('start');
-            print('elevator signal : ' . $this->signal() . "\n");
-            $this->setfloor($this->floor() + 1);
-            print('elevator floor : ' . $this->floor() . "\n");
-            if ($this->floor() == $this->floorMax())
-                $this->setDirection('bottom');
         }
-        $this->setSignal('stop');
-        print('elevator signal : ' . $this->signal() . "\n");
-        $this->setGate('open');
-        print('elevator gate : ' . $this->gate() . "\n");
-        sleep(5);
-        $this->setGate('close');
-        print('elevator gate : ' . $this->gate() . "\n");
     }
 
-    public function goDown(int $floorSelected){
-        while ($this->floor() != $floorSelected) {
-            $this->setDirection('bottom');
+    /**
+     * Sends the elevator to the floor selected or called if the floor is lower than the actual floor of the elevator
+     * @param int $floor  Floor selected or called by the user
+     * 
+     * @return [type]
+     */
+    public function goDown(int $floor){
+        if($this->floor() < $floor)
+            $this->goUp($floor);
+        else{
+            while ($this->floor() != $floor) {
+                $this->setDirection('bottom');
+                $this->setGate('close');
+                $this->setSignal('start');
+                $this->setfloor($this->floor() - 1);
+                if ($this->floor() == $this->floorMin())
+                    $this->setDirection('top');
+            }
+        
+            $this->setSignal('stop');
+            $this->setGate('open');
+            sleep(20);
             $this->setGate('close');
-            $this->setSignal('start');
-            $this->setfloor($this->floor() - 1);
         }
-        $this->setSignal('stop');
-        $this->setGate('open');
-        sleep(40);
-        $this->setGate('close');
     }
 }
 
 class Manager
 {
+    /**
+     * This function signal that the elevator has been call and avoid any duplicate call 
+     * @param int $floorCalled floor called by the user 
+     * @param Elevator $elevator our elevator
+     * 
+     * @return void
+     */
     public function newCall(int $floorCalled, Elevator $elevator){
         $data = $elevator->call();
         if(count($elevator->call()) == 0){
@@ -166,8 +189,15 @@ class Manager
             array_push($data,$floorCalled);
             $elevator->setCall($data);
         }
-        print_r($elevator->call());
     }
+
+    /**
+     * This function give the destination picked by the user to the elevator and verify that the destination isn't already picked
+     * @param int $floor floor picked by the user 
+     * @param Elevator $elevator our elevator
+     * 
+     * @return void
+     */
     public function newDestination(int $floor, Elevator $elevator){
         $data = $elevator->destination();
         if(count($elevator->destination()) == 0){
@@ -183,12 +213,18 @@ class Manager
             array_push($data,$floor);
             $elevator->setdestination($data);
         }
-        print_r($elevator->destination());
     }
 
+    /**
+     * Sends the elevator to the destination picked by the user
+     * @param Elevator $elevator our elevator
+     * @param int $floorSelected floor picked by the user
+     * 
+     * @return void
+     */
     public function goDestination(Elevator $elevator, $floorSelected){
         if($elevator->floor() == $floorSelected){//ascenceur au même niveau que l'étage choisi
-            sleep(20);//attendre 20 secondes
+            sleep(5);//attendre 20 secondes
             $elevator->setGate('close');//fermer la porte
         }
         else if($floorSelected > $elevator->floor())//ascenceur au niveau inférieur de celui appelé
@@ -196,17 +232,87 @@ class Manager
         else//sinon donc ascenceur au niveau supérieur
             $elevator->goDown($floorSelected);//appel goDown()
     }
+
+    /**
+     * Sends the elevator to the floor called 
+     * @param Elevator $elevator our elevator
+     * @param mixed $floorCalled floor called by the user
+     * 
+     * @return [type]
+     */
+    public function goCall(Elevator $elevator, $floorCalled){
+        if($elevator->floor() == $floorCalled){//ascenceur au même niveau que l'étage appelé
+            $elevator->setGate('open');// ouvrir la porte
+            sleep(5);//attendre 40 secondes
+            $elevator->setGate('close');//fermer la porte
+        }
+        else if($floorCalled > $elevator->floor())//ascenceur au niveau inférieur de celui appelé
+            $elevator->goUp($floorCalled);//appel goUp()
+        else//sinon donc ascenceur au niveau supérieur
+            $elevator->goDown($floorCalled);//appel goDown()
+    }
+
+    /**
+     * This function handle all the case our elevator can encoutrer with multiple call and destination 
+     * @param Elevator $elevator our elevator
+     * 
+     * @return void
+     */
     public function goTo(Elevator $elevator){
         $call = $elevator->call();
         $destination = $elevator->destination();
-        for($i=0;$i < count($call); $i++){
-            for($j=0; $j < count($destination); $j++){
-                if($call[$i] !== $destination[$j]){
-                    print("different");
+        while( count($call) !== 0 || count($destination) !== 0){
+            if(count($call)>0 && count($destination) == 0){
+                $floor = array_shift($call);
+                $this->goCall($elevator,$floor);
+                $elevator->setCall($call);
+            }
+            else if(count($call) == 0 && count($destination) > 0){
+                $floor = array_shift($destination);
+                $this->goDestination($elevator,$floor);
+                $elevator->setDestination($destination);
+            }
+            else if(count($call) > 0 && count($destination) > 0){
+                switch($elevator->direction()){
+                    case 'top':
+                        if(min($destination) == array_search(min($destination),$call)){
+                            //$floor = array_shift($destination);
+                            $this->goDestination($elevator,min($destination));
+                            $elevator->setDestination($destination); 
+                            unset($call[array_search(min($destination),$call)]);
+                            $elevator->setCall($call);
+                        }
+                        elseif(min($destination) < min($call)){
+                            //$floor = array_shift($destination);
+                            $this->goDestination($elevator,min($destination));
+                            unset($destination[array_search(min($destination),$destination)]);
+                            $elevator->setDestination($destination);
+                        }
+                        else{
+                            $this->goCall($elevator,min($call));
+                            unset($call[array_search(min($call),$call)]);
+                            $elevator->setCall($call);
+                        }
+                    break;
+                    default :
+                        if($destination[0] == array_search($destination[0],$call)){
+                            $floor = array_shift($destination);
+                            $this->goDestination($elevator,$floor);
+                            $elevator->setDestination($destination); 
+                            unset($call[array_search($floor,$call)]);
+                            $elevator->setCall($call);
+                        }
+                        elseif($destination[0] > max($call)){
+                            $floor = array_shift($destination);
+                            $this->goDestination($elevator,$floor);
+                            $elevator->setDestination($destination);
+                        }
+                        else{
+                            $this->goCall($elevator,max($call));
+                            unset($call[array_search(max($call),$call)]);
+                        }
+
                 }
-                else  
-                    $this->goDestination($elevator, $call[$i], $destination[$j]);
-                    
             }
         }
     }
--- test_elevator.php
+++ test_elevator.php
@@ -1,5 +1,5 @@
 <?php
-require 'fonction.php';
+require 'elevator.php';
 $data = array(
     "id_elevator" => 1,
     "floor" => 0,
@@ -18,60 +18,21 @@ $manager = new Manager();
 
 // $manager->newCall(5, $elevator);
 // $manager->selected(8, $elevator);
-$manager->newCall(5, $elevator);
+
+//Test 1 Appel Unique
+//$manager->newCall(5, $elevator);
+
+//Test 2 Appel && Destination
+//$manager->newCall(2, $elevator);
+//$manager->newDestination(10,$elevator);
+
+//Test 3 multiple appel et destination
 $manager->newCall(10, $elevator);
 $manager->newCall(2, $elevator);
 $manager->newDestination(4, $elevator);
+$manager->goTo($elevator);
 $manager->newDestination(5,$elevator);
 $manager->goTo($elevator);
 // $manager->called(0, $elevator);
 // print_r($elevator);
 
-// public function called(int $floorCalled,Elevator $elevator,){
-//     if($elevator->floor() == $floorCalled){//ascenceur au même niveau que l'étage appelé
-//         $elevator->setGate('open');// ouvrir la porte
-//         sleep(40);//attendre 40 secondes
-//         $elevator->setGate('close');//fermer la porte
-//     }
-//     else if($floorCalled > $elevator->floor())//ascenceur au niveau inférieur de celui appelé
-//         $elevator->goUp($floorCalled);//appel goUp()
-//     else//sinon donc ascenceur au niveau supérieur
-//         $elevator->goDown($floorCalled);//appel goDown()
-// }
-
-// public function selected(int $floorSelected,Elevator $elevator){
-//     if($elevator->floor() == $floorSelected){//ascenceur au même niveau que l'étage choisi
-//         sleep(20);//attendre 20 secondes
-//         $elevator->setGate('close');//fermer la porte
-//     }
-//     else if($floorSelected > $elevator->floor())//ascenceur au niveau inférieur de celui appelé
-//         $elevator->goUp($floorSelected);//appel goUp()
-//     else//sinon donc ascenceur au niveau supérieur
-//         $elevator->goDown($floorSelected);//appel goDown()
-// }
-
-// public function conflictHandle(int $floorCalled, int $floorSelected, Elevator $elevator){
-//     if($floorSelected == $floorCalled)
-//         $this->selected($floorSelected, $elevator);
-    
-//     else if($elevator->direction() == 'top' && $floorSelected < $floorCalled){
-//         $this->selected($floorSelected, $elevator);
-//         $this->called($floorCalled, $elevator);
-//     }
-
-//     else if($elevator->direction() == 'top' && $floorSelected > $floorCalled){
-//         $this->called($floorCalled, $elevator);
-//         $this->selected($floorSelected, $elevator);
-//     }
-
-//     else if($elevator->direction() == 'bottom' && $floorSelected > $floorCalled){
-//         $this->selected($floorSelected, $elevator);
-//         $this->called($floorCalled, $elevator);
-//     }
-
-//     else{
-//         $this->called($floorCalled, $elevator);
-//         $this->selected($floorSelected, $elevator);
-//     }
-
-// }
